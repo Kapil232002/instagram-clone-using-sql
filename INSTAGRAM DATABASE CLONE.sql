53462e2063426690280892b5b09ec9876c1bbc88
kapil232002 <kapilkumar23222@gmail.com>
2025-02-01 00:04:59 +0530

    comment changes

--- INSTAGRAM DATABASE CLONE.sql
+++ INSTAGRAM DATABASE CLONE.sql
@@ -1,18 +1,20 @@
 
--- creation of the database....
+-- CREATION OF DATABASE....
+
 CREATE DATABASE ig_clone2;
 
--- use the database.....
+
+-- USE OF DATABASE.....
+
 USE ig_clone2; 
 
--- lets collect the data and create the tables.....
 
 
--- creation of user tables.....
+-- CREATION OF TABLES.....
 
 CREATE TABLE users (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) UNIQUE NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,                    -- USER TABLE......
     created_at TIMESTAMP DEFAULT NOW()
 );
 
@@ -21,7 +23,7 @@ CREATE TABLE users (
 CREATE TABLE photos (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     image_url VARCHAR(255) NOT NULL,
-    user_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,                                   -- PHOTOS TABLE......    
     created_at TIMESTAMP DEFAULT NOW(),
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
@@ -32,7 +34,7 @@ CREATE TABLE comments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     comment_text VARCHAR(255) NOT NULL,
     photo_id INTEGER NOT NULL,
-    user_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,                                    -- COMMENT TABLE.......
     created_at TIMESTAMP DEFAULT NOW(),
     FOREIGN KEY(photo_id) REFERENCES photos(id),
     FOREIGN KEY(user_id) REFERENCES users(id)
@@ -43,7 +45,7 @@ CREATE TABLE comments (
 CREATE TABLE likes (
     user_id INTEGER NOT NULL,
     photo_id INTEGER NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
+    created_at TIMESTAMP DEFAULT NOW(),                             -- LIKE TABLE.......
     FOREIGN KEY(user_id) REFERENCES users(id),
     FOREIGN KEY(photo_id) REFERENCES photos(id),
     PRIMARY KEY(user_id, photo_id)
@@ -52,7 +54,7 @@ CREATE TABLE likes (
 CREATE TABLE follows (
     follower_id INTEGER NOT NULL,
     followee_id INTEGER NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
+    created_at TIMESTAMP DEFAULT NOW(),                               -- FOLLOW TABLE.......
     FOREIGN KEY(follower_id) REFERENCES users(id),
     FOREIGN KEY(followee_id) REFERENCES users(id),
     PRIMARY KEY(follower_id, followee_id)
@@ -61,7 +63,7 @@ CREATE TABLE follows (
 
 
 CREATE TABLE tags (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,                                -- TAG TABLE........
   tag_name VARCHAR(255) UNIQUE,
   created_at TIMESTAMP DEFAULT NOW()
 );
@@ -69,7 +71,7 @@ CREATE TABLE tags (
 CREATE TABLE photo_tags (
     photo_id INTEGER NOT NULL,
     tag_id INTEGER NOT NULL,
-    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id),                         -- PHOTO TAG TABLE.......
     FOREIGN KEY(tag_id) REFERENCES tags(id),
     PRIMARY KEY(photo_id, tag_id)
 );
